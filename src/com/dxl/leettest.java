d27c6885ad711abde43f92762204df857ae13ea9
Dragon <1030777212@qq.com>
2021-11-22 16:18:15 +0800

    测试项目

--- src/com/dxl/leettest.java
+++ src/com/dxl/leettest.java
@@ -55,7 +55,7 @@ public class leettest {
 
     public static void main(String[] args) {
         leettest l = new leettest();
-        System.out.println(l.minWindow("a", "aaaa"));;
+        System.out.println(l.minWindow("a", "aaaaa"));;
 
     }
 }
